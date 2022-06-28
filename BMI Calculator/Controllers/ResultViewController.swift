18987d39ed72f2e8e8dde2d63ae3388ad323d3e7
Jean Cesca <jeanrcesca@gmail.com>
2022-06-28 20:30:07 -0300

    Adicionado o projeto completo

---
+++ BMI Calculator/Controllers/ResultViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Jean Ricardo Cesca on 14/06/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+    
+    var bmiValue: String?
+    var color: UIColor?
+    var advice: String?
+
+    @IBOutlet weak var bmiLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        view.backgroundColor = color
+    }
+
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+}
